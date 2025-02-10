84e71539ddc2d000b60e529664a95e4b2aec7911
ApurboShib <shibjoy088@gmail.com>
2025-02-10 23:32:42 +0600

    add json convert

---
+++ js/fetch.js
@@ -0,0 +1,20 @@
+// // console.log('fetching..');
+
+// const { json } = require("body-parser");
+// const { response } = require("express");
+
+// fetch('https://jsonplaceholder.typicode.com/todos/1')
+//       .then(response => response.json()) // . jeson is not similar but close to JESON.parse().
+//       .then(json => console.log(json))
+
+ const url = 'https://jsonplaceholder.typicode.com/todos/1';
+// fetch(url)
+//       .then (response => response.json())
+//       .then(json => console.log(json) )
+
+function loadData () {
+      fetch(url)
+      .then (response => response.json())
+      .then(json => console.log(json) )
+
+}
